67f22bb07646f4c91903479ef4e64c8589bb9be3
Danilo <danilodorella2009@gmail.com>
2026-06-03 20:31:52 -0300

    Primeiro commit do JavaMaster

--- src/Sistemas_do_Dia_a_Dia_3/Sistema_de_Reserva_de_Hotel/Application/Main.java
+++ src/Sistemas_do_Dia_a_Dia_3/Sistema_de_Reserva_de_Hotel/Application/Main.java
@@ -1,11 +1,4 @@
 package Sistemas_do_Dia_a_Dia_3.Sistema_de_Reserva_de_Hotel.Application;
 
-import Sistema_de_Reserva_de_Hotel.application.MenuCliente.Menu;
-
 public class Main {
-    public static void main(String[] args) {
-        Menu menuUsuario = new Menu();
-
-        menuUsuario.menuUser();
-    }
 }
